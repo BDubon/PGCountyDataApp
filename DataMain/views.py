af2759c9289fd6fed18be1a4f31b644a64be75d1
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-18 22:06:26 -0500

    New route, installed sodapy

--- DataMain/views.py
+++ DataMain/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def blog(request):
+    return HttpResponse("Cool blog brah,")
