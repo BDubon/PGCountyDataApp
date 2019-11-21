7cfa615f8fe9a3aabb2f75c01845a460d323e57d
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-21 00:33:11 -0500

    Added bootstrap4 to installed apps

--- PGCountyDataApp/settings.py
+++ PGCountyDataApp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DataMain'
+    'DataMain',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
