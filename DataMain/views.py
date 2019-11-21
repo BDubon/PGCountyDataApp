caa7ffd5a0e8636f419275f3e0963b1d450aea93
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-21 01:20:41 -0500

    Added TODO: footer

--- DataMain/views.py
+++ DataMain/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from sodapy import Socrata
 from .credentials import APPTOKEN, USERNAME, PASSWORD
 
-LIMIT = 50       # Total number of records to request from the API
+LIMIT = 5      # Total number of records to request from the API
+
+
 # Create your views here.
 def index(request):
-    client = Socrata('data.princegeorgescountymd.gov',
-                      APPTOKEN,
-                      USERNAME,
-                      PASSWORD)
+    """ Homepage view """
+    client = Socrata('data.princegeorgescountymd.gov', APPTOKEN, USERNAME, PASSWORD)
     results = client.get("364y-gm2b", limit=LIMIT)  # 2017 Data
+    context = {
+        'data': results
+        }
 
-    return render(request, 'index.html', {'data': results})
+    return render(request, 'index.html', context)
 
 
 def blog(request):
-    return HttpResponse("Cool blog brah,")
+    """ Secondary view can be found at /blog """
+    form = 123
+    context = {
+        'form': form
+        }
+
+    return render(request, 'blog.html', context)
