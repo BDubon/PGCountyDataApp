873d0c3ad84503e02271344f9aa4039968ce7ff1
Bryan <33406715+BDubon@users.noreply.github.com>
2019-11-20 22:07:50 -0500

    Added credentials file

---
+++ DataMain/credentials.py
@@ -0,0 +1,7 @@
+# Make sure you go to https://data.princegeorgescountymd.gov/signup 
+# to request a new token and create an account then add them to this file.
+
+APPTOKEN = 'Your App Token'
+USERNAME = 'The email you used to sign up'
+PASSWORD = 'the password you used to sign up'
+
