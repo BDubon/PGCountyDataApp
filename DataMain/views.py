b89776eb76976e02cd511f118b4a88ac54fe80e7
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-20 21:42:49 -0500

    Added api call and passed the data to `main` view

--- DataMain/views.py
+++ DataMain/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sodapy import Socrata
+from .credentials import APPTOKEN, USERNAME, PASSWORD
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    client = Socrata('data.princegeorgescountymd.gov',
+                      APPTOKEN,
+                      USERNAME,
+                      PASSWORD)
+    results = client.get("364y-gm2b", limit=5000)  # 2017 Data
+
+    return render(request, 'index.html', {'data': results})
+
 
 def blog(request):
     return HttpResponse("Cool blog brah,")
