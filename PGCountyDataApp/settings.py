b5f5f831fc0496ea75ac7d5adcb94efd16f487c0
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-22 04:00:47 -0500

    installed `django-feathers` for icons

--- PGCountyDataApp/settings.py
+++ PGCountyDataApp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'DataMain',
-    'bootstrap4'
+    'bootstrap4',
+    'django_feather',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+                    ]
