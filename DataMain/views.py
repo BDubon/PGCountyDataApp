61cc8a624fc14ce97e88764892bb53d28abfbcfc
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-22 04:00:17 -0500

    Added data into table in `/data`

--- DataMain/views.py
+++ DataMain/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from sodapy import Socrata
 from .credentials import APPTOKEN, USERNAME, PASSWORD
 
-LIMIT = 1    # Total number of records to request from the API
+LIMIT = 30   # Total number of records to request from the API
 
 
 # Create your views here.
 def index(request):
-    """ Homepage view """
+    """ Homepage view / """
     context = {
         'title': 'Homepage',
         'section_one': 'Homepage Subtitle',
@@ -19,7 +19,7 @@ def index(request):
 
 
 def data(request):
-    """ Secondary view can be found at /blog """
+    """ Secondary view can be found at /data """
     client = Socrata('data.princegeorgescountymd.gov', APPTOKEN, USERNAME, PASSWORD)
     received_data = client.get("364y-gm2b", limit=LIMIT)  # 2017 Data
 
