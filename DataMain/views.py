162af917a6a32878077658d27a8ef6114fba855d
BDubon <33406715+BDubon@users.noreply.github.com>
2019-11-22 02:06:31 -0500

    Playing with the `context` object and variables

--- DataMain/views.py
+++ DataMain/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from sodapy import Socrata
 from .credentials import APPTOKEN, USERNAME, PASSWORD
 
-LIMIT = 5      # Total number of records to request from the API
+LIMIT = 5000     # Total number of records to request from the API
 
 
 # Create your views here.
@@ -11,7 +12,9 @@ def index(request):
     client = Socrata('data.princegeorgescountymd.gov', APPTOKEN, USERNAME, PASSWORD)
     results = client.get("364y-gm2b", limit=LIMIT)  # 2017 Data
     context = {
-        'data': results
+        'data': results,
+        'title': 'Homepage',
+        'section_one': 'Homepage Subtitle',
         }
 
     return render(request, 'index.html', context)
@@ -21,7 +24,9 @@ def blog(request):
     """ Secondary view can be found at /blog """
     form = 123
     context = {
-        'form': form
+        'title': 'Blog',
+        'form': form,
+        'section_one': 'Blog Subtitle',
         }
 
     return render(request, 'blog.html', context)
