343743dc132e479838652ec5ee6564e62adaa01a
Bryan <33406715+BDubon@users.noreply.github.com>
2019-11-23 04:23:40 -0500

    Update settings.py

--- PGCountyDataApp/settings.py
+++ PGCountyDataApp/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE_CLASSES = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'PGCountyDataApp.urls'
 
