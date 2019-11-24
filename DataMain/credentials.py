b58f4406019e41db2989c54d6d1e9d98933fc9ea
Bryan <33406715+BDubon@users.noreply.github.com>
2019-11-24 02:18:04 -0500

    Create credentials.py

---
+++ DataMain/credentials.py
@@ -0,0 +1,10 @@
+# ** SODAPY CREDENTIALS **
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(data.princegeorgescountymd.gov,
+#                  MyAppToken,
+#                  userame="user@example.com",
+#                  password="AFakePassword")
+
+APPTOKEN = 'eMFzKSqlHamPQEU4Erg7w8q8D'
+USERNAME = 'bryangomez_2006@yahoo.com'
+PASSWORD = 'RandomPass4321'
